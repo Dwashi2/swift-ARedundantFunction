c9fa2d67879c9e17c29743221331d1702a46d139
Daniel Washington <danielvertigo@hotmail.com>
2021-05-31 14:02:17 -0300

    Files

--- ARedundantFunction/ViewController.swift
+++ ARedundantFunction/ViewController.swift
@@ -5,6 +5,25 @@
 //  Created by Daniel Washington Ignacio on 31/05/21.
 //
 
+
+/*
+ Write a function that takes in a string s and returns a function that returns s.
+
+ Examples
+
+ let f1 = redundant("apple")
+ f1() ➞ "apple"
+
+ let f2 = redundant("pear")
+ f2() ➞ "pear"
+
+ let f3 = redundant("")
+ f3() ➞ ""
+ Notes
+
+ Your function should return a function, not a string.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +31,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let f1 = redundant("apple")
+        let f2 = redundant("pear")
+        let f3 = redundant("")
+        print(f1())
+        print(f2())
+        print(f3())
     }
 
+    
+    func redundant(_ s: String) -> () -> String {
+        return {s}
+    }
 
 }
 
